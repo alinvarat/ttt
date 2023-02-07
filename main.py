1ef38b164673b003c84f138792887581a7e34cbe
alinvarat <93310135+alinvarat@users.noreply.github.com>
2023-02-07 14:44:05 +0700

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
-USR = 0
-USL = 0
-USC = 0
-ENCL = 0
-count = 0
+USC = int(0)
 
 def on_forever():
-    global USC, USL, USR, ENCL, count
-    USC = Math.idiv(iBIT.read_adc(ibitReadADC.ADC1), 10)
-    USL = Math.idiv(iBIT.read_adc(ibitReadADC.ADC0), 10)
-    USR = Math.idiv(iBIT.read_adc(ibitReadADC.ADC2), 10)
-    ENCL = Math.idiv(iBIT.read_adc(ibitReadADC.ADC3), 10)
-    ENCL = 0
-    ENCL = 0
-    for ENCL in range (400):
-        if ENCL > 56:
-            count += 1
-    serial.write_value("V", count)
+    global USC
+    USC = iBIT.read_adc(int(ibitReadADC.ADC1))
+    serial.write_value("USC", int(USC))
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,14 +1,5 @@
-let USR = 0
-let USL = 0
 let USC = 0
-let count = 0
-let ENCL = 0
 basic.forever(function () {
-    USC = Math.idiv(iBIT.ReadADC(ibitReadADC.ADC1), 10)
-    USL = Math.idiv(iBIT.ReadADC(ibitReadADC.ADC0), 10)
-    USR = Math.idiv(iBIT.ReadADC(ibitReadADC.ADC2), 10)
-    ENCL = Math.idiv(iBIT.ReadADC(ibitReadADC.ADC3), 10)
-    ENCL = 0
-    ENCL = 0
-    serial.writeValue("V", count)
+    USC = iBIT.ReadADC(ibitReadADC.ADC1)
+    serial.writeValue("USC", USC)
 })
